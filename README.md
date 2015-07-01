14471301ae9454f785f6013e37a50a367e410798
Matthew Kaufer <mjkaufer@gmail.com>
2015-07-01 15:51:55 -0400

    Basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Messer
 Command-line messaging for Facebook Messenger
+
+## Installation
+Install `messer` globally with `npm install messer -g`, so you can use the command in any context.
+
+## Setup
+
+Create a `config.json` somewhere. Inside of the `config.json`, add
+
+```
+{
+	"email": "email",
+	"password": "password"
+}
+```
+Fill in the email you use for facebook, along with your facebook password
+
+## Usage
+Simply type `messer path/to/config.json`, replacing `path/to/config.json` with the path to your `config.json`
+
+Once you're logged in, you'll see a REPL.
+
+Currently, the only supported command is `message`
+
+### Syntax:
+
+`message "[user]" message`
+
+The quotes around the username are mandatory. An example might be `message "Matthew" hello world!`
+
+When sending a message, Messer picks the closest match to what you type in as `user`. For this reason, you should probably use last names if you want to avoid accidentally texting someone.
+
+## Contributing
+
+Send a pull request!
---
+++ index.js
@@ -0,0 +1,99 @@
+#! /usr/bin/env node
+
+var login = require("facebook-chat-api");
+var fs = require('fs')
+var repl = require('repl')
+
+
+if(process.argv.length < 3)
+	return console.log("Please specify a config JSON as your second argument!")
+
+
+fs.readFile(process.argv[2], function(err, data){
+	if(err)
+		return console.log(err)
+
+
+	var json = JSON.parse(data)
+
+	login(json, function(err, api) {
+
+	    if(err) return console.error(err);
+
+	    console.log("Logged in as " + json.email)
+
+	    api.setOptions({
+	    	logLevel: "silent"
+	    })
+
+	    api.listen(function callback(err, message) {
+	    	process.stderr.write("\007");//makes a beep
+	    	if(message.type == "sticker"){
+	    		console.log("New sticker from " + message.sender_name + " - Sticker URL: " + message.sticker_url)
+	    	}
+	    	console.log("New message from " + message.sender_name + " - " + message.body)
+
+	    });
+
+		var quoteReg = /(".*?")(.*)/g
+	    repl.start({
+	    	ignoreUndefined: true,
+	    	eval: function(cmd, context, filename, callback){
+	    		var ndex = cmd.lastIndexOf("\n")
+	    		ndex = ndex == -1 ? cmd.length : ndex
+
+	    		cmd = cmd.substring(0, ndex)
+
+
+	    		if(cmd.toLowerCase() == "help"){
+	    			showHelp()
+	    			return callback(null)
+	    		} else if(cmd.toLowerCase().indexOf("message") == 0){
+	    			cmd = cmd.substring("message".length).trim()
+
+	    			if(cmd.match(quoteReg) == null){
+	    				console.log("Invalid message - check your syntax")
+	    				showHelp()
+	    				return callback(null)
+	    			}
+
+	    			var decomposed = quoteReg.exec(cmd)
+	    			var to = decomposed[1].replace(/"/g,"")
+	    			var message = decomposed[2].trim()
+	    			if(message.length == 0){
+	    				console.log("No message to send - check your syntax")
+	    				showHelp()
+	    				return callback(null)
+	    			}
+
+	    			api.sendDirectMessage(message, to, function(err, data){
+	    				if(err){
+	    					console.log("ERROR!")
+	    					console.log(err)
+	    					return callback(null)
+	    				}
+
+	    				console.log("Sent message to " + to)
+	    				return callback(null)
+	    			})
+
+	    		} else {
+	    			console.log("Invalid command - check your syntax")
+	    			showHelp()
+	    			return callback(null)	
+	    		}
+
+	    	}
+	    })
+
+	    function showHelp(){
+		    console.log("Commands:\n" + 
+				" message \"[user]\" [message]"
+			)
+	    }
+
+	});
+})
+
+
+		

e1de485ce8bf4e6abdc2c6268058bbabb54c316e
tomquirk <tom@tqstudios.org>
2016-12-07 11:57:30 +1000

    es6 + eslint

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
+"use strict"
 
-/* imports */
+/* Imports */
 const repl = require("repl")
-const chalk = require("chalk")
-const commands = require("./commands")
-const Session = require("./session")
+const facebook = require("facebook-chat-api")
+
+/* Globals */
+let api = {}
+let user = {}
+let lastThread = null
 
 /* Command type constants */
 const commandEnum = {
@@ -13,18 +17,15 @@ const commandEnum = {
 	CONTACTS: "contacts",
 	HELP: "help"
 }
-
+var test = "asd"
 const commandMap = {
 	"r": commandEnum.REPLY,
 	"m": commandEnum.MESSAGE
 }
 
-const colors = ["green", "yellow", "blue", "magenta", "cyan", "red"]
-
 /* Initialisation */
 if (process.argv.length < 3) {
 	//	User didn't store credentials in JSON, make them manually enter credentials
-
 	const prompt = require("prompt")
 	console.log("Enter your Facebook credentials - your password will not be visible as you type it in")
 	prompt.start()
@@ -35,18 +36,13 @@ if (process.argv.length < 3) {
 	}, {
 		name: "password",
 		hidden: true,
-		conform: function(value) {
-			return true
-		}
-	}], function(err, result) {
-		authenticate(result)
-	})
+		conform() { return true }
+	}], (err, result) => { authenticate(result) })
 
 } else {
 	const fs = require("fs")
-	fs.readFile(process.argv[2], function(err, data) {
-		if (err)
-			return console.log(err)
+	fs.readFile(process.argv[2], (err, data) => {
+		if (err) return console.log(err)
 
 		authenticate(JSON.parse(data))
 	})
@@ -57,9 +53,12 @@ if (process.argv.length < 3) {
  */
 function getUserDetails() {
 	console.info("Fetching user details...")
-	return promise = new Promise(function(resolve, reject) {
-		api.getFriendsList(function(err, data) {
-			if (err) return console.error(err)
+	return new Promise((resolve, reject) => {
+		api.getFriendsList((err, data) => {
+			if (err) {
+				console.error(err)
+				reject()
+			}
 			user.friendsList = data
 			resolve()
 		})
@@ -73,34 +72,26 @@ function handleMessage(message) {
 	var unrenderableMessage = ", unrenderable in Messer :("
 
 	// seen message (not sent)
-	if (!message.senderID)
+	if (!message.senderID || message.type != "message")
 		return
 
-	let sender = user.friendsList.find(function(f) { return f.userID === message.senderID })
-	sender = sender.fullName || "Unknown User";
+	let sender = user.friendsList.find(f => { return f.userID === message.senderID })
+	sender = sender.fullName || "Unknown User"
 
 	if (message.participantNames && message.participantNames.length > 1)
 		sender = "'" + sender + "'" + " (" + message.senderName + ")"
 
-	process.stderr.write("\007")	// Terminal notification
+	process.stderr.write("\x07")	// Terminal notification
 
 	let messageBody = null
 
-	if (message.type != "message") {
-		return
-	}
-	else if (message.body !== undefined && message.body != "") {
+	if (message.body !== undefined && message.body != "") {
 		// console.log("New message sender " + sender + " - " + message.body)
 		messageBody = " - " + message.body
 	}
 
-	// add to active conversations if it is new
-	if (!activeConversations.indexOf(sender))
-		activeConversations.push(sender)
-
 	if (message.attachments.length == 0) {
-		const color = colors[activeConversations.length % colors.length]
-		console.log(chalk[color]("New message from " + sender + (messageBody || unrenderableMessage)))
+		console.log("New message from " + sender + (messageBody || unrenderableMessage))
 	} else {
 		const attachment = message.attachments[0]//only first attachment
 		const attachmentType = attachment.type.replace(/\_/g, " ")
@@ -110,11 +101,91 @@ function handleMessage(message) {
 	lastThread = message.threadID
 }
 
+/* command handlers */
+const commands = {
+  /**
+   * Sends message to given user
+   */
+	message(rawCommand) {
+		const quoteReg = /(".*?")(.*)/g
+		// to get length of first arg
+		const args = rawCommand.replace("\n", "").split(" ")
+		const cmd = rawCommand.substring(args[0].length).trim()
+
+		if (cmd.match(quoteReg) == null) {
+			console.warn("Invalid message - check your syntax")
+			return processCommand("help")
+		}
+
+		const decomposed = quoteReg.exec(cmd)
+		const rawReceiver = decomposed[1].replace(/"/g, "")
+		const message = decomposed[2].trim()
+
+		if (message.length == 0) {
+			console.warn("No message to send - check your syntax")
+			return processCommand("help")
+		}
+
+		// Find the given reciever in the users friendlist
+		const receiver = user.friendsList.find(f => {
+			return f.fullName.toLowerCase().startsWith(rawReceiver.toLowerCase())
+		})
+
+		if (!receiver) {
+			console.warn("User \"" + rawReceiver + "\"" + " could not be found in your friends list!")
+			return
+		}
+
+		api.sendMessage(message, receiver.userID, err => {
+			if (err) console.warn("ERROR!", err)
+
+			console.log("Sent message to " + receiver.fullName)
+		})
+	},
+
+  /**
+   * Replies with a given message to the last received thread.
+   */
+	reply(rawCommand) {
+		if (lastThread === null) {
+			console.warn("Error - can't reply to messages you haven't yet received! You need to receive a message before using `reply`!")
+		}
+
+		const args = rawCommand.replace("\n", "").split(" ")
+		const body = rawCommand.substring(args[0].length).trim()
+
+		// var body = rawCommand.substring(commandEnum.REPLY.length).trim()
+
+		api.sendMessage(body, lastThread, err => {
+			if (err) return console.error(err)
+
+			console.log("✓")
+		})
+	},
+
+  /**
+   * Displays users friend list
+   */
+	contacts() {
+		user.friendsList.forEach(f => { console.log(f.fullName) })
+	},
+
+  /**
+   * Displays usage instructions
+   */
+	help() {
+		console.log("Commands:\n" +
+			"\tmessage \"[user]\" [message]\n" +
+			"\tcontacts\n"
+		)
+	}
+}
+
 /**
  * Execute appropriate action for user input commands
  */
-function processCommand(rawCommand, cb) {
-	const args = rawCommand.replace('\n', '').split(' ')
+function processCommand(rawCommand) {
+	const args = rawCommand.replace("\n", "").split(" ")
 	const command = commandMap[args[0]] || args[0]
 	const commandHandler = commands[command]
 
@@ -123,9 +194,34 @@ function processCommand(rawCommand, cb) {
 	} else {
 		commandHandler(rawCommand)
 	}
-
-	if (cb)
-		return cb(null)
 }
 
-// new Session(credentials)
+function authenticate(credentials) {
+	facebook(credentials, (err, fbApi) => {
+		if (err) return console.error(err)
+
+		api = fbApi // assign to global variable
+		api.setOptions({ logLevel: "silent" })
+
+		console.info("Logged in as " + credentials.email)
+
+		getUserDetails(api, user).then(() => {
+			console.info("Listening for incoming messages...")
+
+			// listen for incoming messages
+			api.listen((err, message) => {
+				if (err) return console.error(err)
+				handleMessage(message)
+			})
+
+			// start REPL
+			repl.start({
+				ignoreUndefined: true,
+				eval(cmd) {
+					processCommand(cmd)
+				}
+			})
+		})
+
+	})
+}

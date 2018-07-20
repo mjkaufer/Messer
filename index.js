84cb8587f9e0c553e56f612b868556b1ead77b7c
SUBmarinoff <SUBmarinoff@users.noreply.github.com>
2018-07-20 14:54:33 +0300

    Fixed typo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Messer = require("./src/messer")
 
 const messer = new Messer()
 if (process.argv.length > 3) {
-  messer.startSingle(process.argv.slice(3, process.argv.legnth).join(" "))
+  messer.startSingle(process.argv.slice(3, process.argv.length).join(" "))
 } else {
   messer.start()
 }

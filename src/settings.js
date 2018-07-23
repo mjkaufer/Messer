37cc035b78f1c4cdec313814b3262d2ea9be62ff
Tom Quirk <tomquirkacc@gmail.com>
2018-07-23 21:54:36 +1000

    change appstate parent dir

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 const path = require("path")
 
-const APPSTATE_FILE_PATH = path.resolve(__dirname, "tmp/appstate.json")
+const APPSTATE_FILE_PATH = path.resolve(process.env.HOME, ".messer/appstate.json")
 
 module.exports = {
   APPSTATE_FILE_PATH,
